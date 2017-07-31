2aeda6d0238bc8d98f76ba8fee79e623e8df26e7
Antifreez <antifreez@gmail.com>
2017-07-31 23:37:57 +0300

    Strated toolkit implimentation

--- src/Toolbox.js
+++ src/Toolbox.js
@@ -3,14 +3,29 @@ import { Button, Form, FormGroup, Input } from 'reactstrap';
 import './styles/toolbox.css';
 
 class Toolbox extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        contact:''
+        }
+    }
+
+    addContact(){
+        console.log("Add conatct");
+    }
+
+    onContactInputChange(e){
+        console.log("input changed " + e.target.value)
+    }
+
     render() {
         return (
             <div className='toolbox'>
                 <Form inline>
                     <FormGroup>
-                        <Input type="text" name="email" id="exampleEmail" placeholder="New contact" />
+                        <Input size="sm" type="text" onChange={this.onContactInputChange} placeholder="New contact" />
                     </FormGroup>
-                    <Button>Add Conatct</Button>
+                    <Button size="sm" onClick={this.addContact}>Add Conatct</Button>
                 </Form>
             </div>
         )
